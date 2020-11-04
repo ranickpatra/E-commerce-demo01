c407325fc60db4352f68bf6bbe49ddafb00df859
ranickpatra <ranickpatra.nalikul@gmail.com>
2020-11-04 09:43:04 +0530

    changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './head/header/header.component';
-import { HomeComponent } from './body/home/home.component';
-import { ProductCard1Component } from './body/cards/product-card1/product-card1.component';
-import { ProductCard2Component } from './body/cards/product-card2/product-card2.component';
-import { ProductCard3Component } from './body/cards/product-card3/product-card3.component';
-import { BannerCardSmallComponent } from './body/cards/banner-card-small/banner-card-small.component';
-import { BannerCardSmallFullWidthComponent } from './body/cards/banner-card-small-full-width/banner-card-small-full-width.component';
-import { BannerCardMediumComponent } from './body/cards/banner-card-medium/banner-card-medium.component';
-import { ProductCard4Component } from './body/cards/product-card4/product-card4.component';
-import { BannerCardMediumSmallComponent } from './body/cards/banner-card-medium-small/banner-card-medium-small.component';
-import { CategoriesCardComponent } from './body/cards/categories-card/categories-card.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './app-body/home/home.component';
+import { ProductCard1Component } from './app-body/cards/product-card1/product-card1.component';
+import { ProductCard2Component } from './app-body/cards/product-card2/product-card2.component';
+import { ProductCard3Component } from './app-body/cards/product-card3/product-card3.component';
+import { BannerCardSmallComponent } from './app-body/cards/banner-card-small/banner-card-small.component';
+import { BannerCardSmallFullWidthComponent } from './app-body/cards/banner-card-small-full-width/banner-card-small-full-width.component';
+import { BannerCardMediumComponent } from './app-body/cards/banner-card-medium/banner-card-medium.component';
+import { ProductCard4Component } from './app-body/cards/product-card4/product-card4.component';
+import { BannerCardMediumSmallComponent } from './app-body/cards/banner-card-medium-small/banner-card-medium-small.component';
+import { CategoriesCardComponent } from './app-body/cards/categories-card/categories-card.component';
+import { AppBodyComponent } from './app-body/app-body.component';
+import { WishListComponent } from './app-body/wish-list/wish-list.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,8 @@ import { CategoriesCardComponent } from './body/cards/categories-card/categories
     ProductCard4Component,
     BannerCardMediumSmallComponent,
     CategoriesCardComponent,
+    AppBodyComponent,
+    WishListComponent,
   ],
   imports: [
     BrowserModule,
