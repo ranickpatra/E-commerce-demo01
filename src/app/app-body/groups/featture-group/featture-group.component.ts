27436135b3c0c05173195699d9724e7390c34138
ranickpatra <ranickpatra.nalikul@gmail.com>
2020-11-04 15:54:20 +0530

    updated

---
+++ src/app/app-body/groups/featture-group/featture-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featture-group',
+  templateUrl: './featture-group.component.html',
+  styleUrls: ['./featture-group.component.css']
+})
+export class FeattureGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
