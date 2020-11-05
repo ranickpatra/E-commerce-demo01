3e860c48078de916cbc2038de43cf43197f4c512
ranickpatra <ranickpatra.nalikul@gmail.com>
2020-11-05 09:33:16 +0530

    updated pricing

--- src/app/app-body/components/price/price-component1/price-component1.component.ts
+++ src/app/app-body/components/price/price-component1/price-component1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-price-component1',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceComponent1Component implements OnInit {
 
+  @Input() realPrice: number;
+  @Input() salePrice: number;
+  saleRate: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.salePrice )
+      this.saleRate = this.salePrice / this.realPrice;
   }
 
 }
--- src/app/app-body/components/ratting/product-ratting-view/product-ratting-view.component.ts
+++ src/app/app-body/components/ratting/product-ratting-view/product-ratting-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-ratting-view',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductRattingViewComponent implements OnInit {
 
+  @Input() ratting: number;
+  @Input() numberOfRatting: number;
+  
   constructor() { }
 
   ngOnInit(): void {
