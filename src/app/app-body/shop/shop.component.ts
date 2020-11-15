bee087ee2338e845f5b7534e2cc60beb4fddd942
ranickpatra <ranickpatra.nalikul@gmail.com>
2020-11-15 13:43:28 +0530

    filter done

--- src/app/app-body/shop/shop.component.ts
+++ src/app/app-body/shop/shop.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-shop',
@@ -12,14 +11,12 @@ export class ShopComponent implements OnInit {
   currentPage: number = 2;
   numOfPages = 8;
   pages: Array<number> = [];
-  items = []
+  items = [];
+
+  filterState: boolean = false;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-    // let page = activatedRoute.snapshot.params.page;
-    // this.currentPage = page ? page as unknown as number: 1;
-    // console.log(this.currentPage);
-
-    let firstTime = true;
+    let firstTime: boolean = true;
     activatedRoute.params.subscribe(params => {
       let page: number = parseInt(params.page);
       this.currentPage = page ? page : 1;
@@ -46,6 +43,12 @@ export class ShopComponent implements OnInit {
     console.log("init page called")
     this.pages = [];
     this.items = new Array(Math.floor(Math.random() * 8) + 10);
+  }
+
+
+  // chnage filter state
+  showFilter(status) {
+    this.filterState = status;
   }
 
 
